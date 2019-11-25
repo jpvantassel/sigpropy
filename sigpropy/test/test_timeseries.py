:bug: fix switch arange to linspace due to float precision

sigpropy/test/test_timeseries.py
@@ -71,6 +71,25 @@ class TestTimeSeries(unittest.TestCase):
         test = sigpropy.TimeSeries(amp, dt, delay=-0.5)
         self.assertListEqual(test.time.tolist(), true_time)
 
+        # 2d amp
+        dt = 1
+        amp = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        test = sigpropy.TimeSeries(amp, dt)
+        test.split(3)
+        self.assertListEqual(test.time.tolist(), [[0, 1, 2, 3],
+                                                  [3, 4, 5, 6],
+                                                  [6, 7, 8, 9]])
+        self.assertEqual(test.time.size, test.amp.size)
+
+        # 2d amp
+        dt = 1
+        amp = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        test = sigpropy.TimeSeries(amp, dt)
+        test.split(4)
+        self.assertListEqual(test.time.tolist(), [[0, 1, 2, 3, 4],
+                                                  [4, 5, 6, 7, 8]])
+        self.assertEqual(test.time.size, test.amp.size)
+
     def test_split(self):
         amp = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         dt = 1
@@ -192,8 +211,8 @@ class TestTimeSeries(unittest.TestCase):
 
     def test_detrend(self):
         # 1d amp
-        signal =np.array([0., .2, 0., -.2]*5)
-        trend = np.arange(0,20,1)
+        signal = np.array([0., .2, 0., -.2]*5)
+        trend = np.arange(0, 20, 1)
         amp = signal + trend
         dt = 1
         tseries = sigpropy.TimeSeries(amp, dt)
@@ -203,14 +222,13 @@ class TestTimeSeries(unittest.TestCase):
 
         # 2d amp
         signal = np.array([0., .2, 0., -.2]*5)
-        trend = np.arange(0,20,1)
+        trend = np.arange(0, 20, 1)
         amp = signal + trend
         amp = np.vstack((amp, amp))
         dt = 1
         tseries = sigpropy.TimeSeries(amp, dt)
         tseries.detrend()
         for row in tseries.amp:
-            print(row)
             for true, test in zip(signal, row):
                 self.assertAlmostEqual(true, test, delta=0.03)
 

sigpropy/timeseries.py
@@ -115,7 +115,7 @@ class TimeSeries():
         self.fnyq = 0.5*self.fs
         self._df = self.fs/self.n_samples
         self._nstack = 1
-        if delay>0:
+        if delay > 0:
             raise ValueError("`delay` must not be > 0")
         self.delay = delay
         self.multiple = 1
@@ -129,7 +129,16 @@ class TimeSeries():
     @property
     def time(self):
         """Return time vector for TimeSeries object."""
-        return np.arange(0, self.n_samples*self.dt, self.dt) + self.delay
+        if self.n_windows == 1:
+            return np.arange(0, self.n_samples*self.dt, self.dt) + self.delay
+        else:
+            samples_per_window = (self.n_samples//self.n_windows)+1
+            time = np.zeros((self.n_windows, samples_per_window))
+            for cwindow in range(self.n_windows):
+                start_time = cwindow*(samples_per_window-1)*self.dt
+                stop_time = start_time + (samples_per_window-1)*self.dt
+                time[cwindow] = np.linspace(start_time, stop_time, samples_per_window)
+            return time
 
     def trim(self, start_time, end_time):
         """Trim excess from time series in the half-open interval
@@ -217,8 +226,8 @@ class TimeSeries():
                                        np.zeros(nreq - self.n_samples)))
             self.n_samples = nreq
 
-        # If nreq <= n_samples, padd zeros to achieve a fraction of df 
-        # (e.g., df/2). After processing, extract the results at the 
+        # If nreq <= n_samples, padd zeros to achieve a fraction of df
+        # (e.g., df/2). After processing, extract the results at the
         # frequencies of interest
         else:
             # Multiples of df and nreq
@@ -236,14 +245,14 @@ class TimeSeries():
             self.n_samples = nreq*self.multiple
 
             logging.debug(f"n_samples = {self.n_samples}")
-        
+
     def detrend(self):
         """Remove linear trend from time series.
 
         Returns:
             `None`, remove linear trend from attribute `amp`.
         """
-        if self.n_windows==1:
+        if self.n_windows == 1:
             self.amp = detrend(self.amp)
         else:
             for row, amp in enumerate(self.amp):