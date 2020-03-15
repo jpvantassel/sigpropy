54104033ba9d155bd83de23dbe5b29f82d65d03e
Joseph vantassel <jvantassel@utexas.edu>
2020-03-15 17:12:09 -0500

    :racehorse: Performance benchmark

--- test/perf_process.py
+++ test/perf_process.py
@@ -19,23 +19,31 @@
 
 import sigpropy as sp
 import obspy
+import numpy as np
 import cProfile
 import pstats
 from testtools import get_full_path
 
 full_path = get_full_path(__file__)
 
+
 def main():
     fname = full_path+"data/a2/UT.STN11.A2_C50.miniseed"
     traces = obspy.read(fname)
-    timeseries = sp.TimeSeries.from_trace(traces[0])
-    timeseries.split(windowlength=120)
-    timeseries.bandpassfilter(flow=0.2, fhigh=45, order=5)
-    timeseries.cosine_taper(width=0.2)
+    tseries = sp.TimeSeries.from_trace(traces[0])
+    wtseries = sp.WindowedTimeSeries.from_timeseries(tseries, windowlength=360)
+    wtseries.bandpassfilter(flow=0.2, fhigh=45, order=5)
+    wtseries.cosine_taper(width=0.2)
+    fft = sp.FourierTransformSuite.from_timeseries(wtseries)
 
-    fft = sp.FourierTransform.from_timeseries(timeseries)
-    fft.smooth_konno_ohmachi(bandwidth=40.)
-    fft.resample(minf=0.1, maxf=50, nf=512, res_type="log", inplace=True)
+    # Slow smoothing used pre 15 March 2020
+    # fft.smooth_konno_ohmachi(bandwidth=40.)
+    # fft.resample(minf=0.1, maxf=50, nf=512, res_type="log", inplace=True)
+
+    # Fast smoothing used post 15 March 2020
+    frequencies = np.logspace(np.log10(0.1), np.log10(50), 512)
+    fft.smooth_konno_ohmachi_fast(frequencies=frequencies, bandwidth=40.)
+
 
 fname = full_path+".tmp_profiler_run"
 data = cProfile.run('main()', filename=fname)
@@ -47,4 +55,6 @@ stat.print_stats(0.01)
 # -------------------------
 # 2020 - 02 - 10 :  1.532s -> Baseline
 # 2020 - 02 - 23 :  1.282s -> empty_like
-
+# 2020 - 03 - 15 :  1.401s -> Refactor tseries and fft
+# 2020 - 03 - 15 :  16.47s -> Change from 120s to 360s
+# 2020 - 03 - 15 :  1.342s -> Fast smoothing
